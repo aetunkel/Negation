501ac3e95c30f94b34d7a92a21cd4cb6707f9b0b
Alex Tunkel <aetunkel@gmail.com>
2019-07-09 11:05:02 -0500

    initial commit

--- 04_trials.js
+++ 04_trials.js
@@ -1,21 +1,34 @@
 // In this file you can specify the trial data for your experiment
 
+var unkImg = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+var knownImg = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
+
+shuffle = function (o) { //v1.0
+   for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+   return o;
+}
+
+shuffle(unkImg)
+shuffle(knownImg)
+
 
 const trial_info = {
-    forced_choice: [
+    textbox_input: [
+      // 1
         {
-            question: "What's on the bread?",
-            picture: "images/question_mark_02.png",
-            option1: 'jam',
-            option2: 'ham',
-            correct: 'jam'
+            question: "Type a message to your partner:",
+            picture1: "img/A.jpg",
+            picture2: "img/1.jpg",
+            min_chars: 5
         },
+
+    // 2
+
         {
-            question: "What's the weather like?",
-            picture: "images/weather.jpg",
-            option1: "shiny",
-            option2: "rainbow",
-            correct: "shiny"
+          question: "Type a message to your partner:",
+          picture1: "img/1.jpg",
+          picture2: "img/2.jpg",
+          min_chars: 5
         }
     ]
 };
--- 05_views.js
+++ 05_views.js
@@ -23,27 +23,68 @@ const intro = babeViews.view_generator("intro",{
     trials: 1,
     name: 'intro',
     // If you use JavaScripts Template String `I am a Template String`, you can use HTML <></> and javascript ${} inside
-    text:   `This is a sample introduction view.
-            <br />
-            <br />
-            The introduction view welcomes the participant and gives general information
-            about the experiment. You are in the <strong>${coin}</strong> group.
-            <br />
-            <br />
-            This is a minimal experiment with one forced choice view. It can serve as a starting point for programming your own experiment.`,
-   buttonText: 'Begin the experiment'    
+    text:   ` <strong> RISKS AND BENEFITS: </strong> The risks to your participation in this online study are those associated with basic computer tasks, including boredom, fatigue, mild stress, or breach of confidentiality. The only benefit to you is the learning experience from participating in a research study.
+            <br /><br />
+            <strong> COMPENSATION: </strong> You will receive $0.50 for your participation. NOTE: This study contains comprehension checks to make sure that participants are paying attention.  As long as you read the instructions and complete the tasks, your HIT will be approved.  If you fail these checks, your HIT will be rejected.
+            <br /><br />
+            <strong> CONFIDENTIALITY </strong>: Your Mechanical Turk Worker ID will be used to distribute payment to you but will not be stored with the research data we collect from you.  Please be aware that your MTurk Worker ID can potentially be linked to information about you on your Amazon public profile page, depending on the settings you have for your Amazon profile.  We will not be accessing any personally identifying information about you that you may have put on your Amazon public. profile page.
+            <br /><br />
+            The research is being conducted with the goal of publication in academic journals and presentations at academic conferences. Any reports and presentations about the findings from this study will not include your name or any other information that could identify you. We may share the data we collect in this study with other researchers– again, we will not include information that could identify you.
+            <br /><br />
+            <strong>SUBJECT’S RIGHTS:</strong> Your participation is voluntary.  You may stop participating at any time by closing the browser window or the program to withdraw from the study.
+            <br /><br />
+            <strong>CONTACT:</strong>  If you have any questions, comments, or concerns please contact Daniel Yurovsky at yurovsky@uchicago.edu.
+            <br /> <br />
+            For questions about your rights as a research participant, you may contact:<br /><br />
+    				<strong> The Social and Behavioral Sciences Institutional Review Board </strong> <br />
+    				University of Chicago<br/>
+    				Phone: (773) 834-7835<br/>
+    				E-mail: sbs-irb@uchicago.edu`,
+   buttonText: 'I consent to participate'
 });
 
 // For most tasks, you need instructions views
 const instructions = babeViews.view_generator("instructions",{
     trials: 1,
     name: 'instrucions',
-    title: 'General Instructions',
-    text:  `This is a sample instructions view.
-            <br />
-            <br />
-            Tell your participants what they are to do here.`,
-    buttonText: 'go to trials'
+    title: 'Game Instructions',
+    text:  ` You will be helping a partner choose between two objects by typing a message into a textbox. <br /><br />
+            A <strong style="color:blue;"> blue square</strong> will indicate the object to discuss. <br /><br />
+            Your partner will later use your message to select between the same two objects, which may be in a different order.
+           <br /><br />
+
+           Please stay on topic. Random responses will result in rejected Hits. <br /> <br />
+
+          <i>These are images you will see in this experiment. Some may appear more than others.</i> <br /> <br />
+
+ <div style='width: 800px; margin: 0 auto; text-align: center'>
+     <div class="row">
+       <div class="column"><img src='img/1.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/A.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/2.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/B.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/3.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/C.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/4.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/D.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/5.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/E.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/6.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/F.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/7.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/G.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/8.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/H.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/9.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/I.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/10.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/J.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/11.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/K.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/12.jpg' width="100" height="100"></img></div>
+       <div class="column"><img src='img/L.jpg' width="100" height="100"></img></div>
+   </div></div> `,
+    buttonText: 'Begin'
 });
 
 
@@ -103,12 +144,12 @@ const thanks = babeViews.view_generator("thanks", {
 
 
 // Here, we initialize a normal forced_choice view
-const forced_choice_2A = babeViews.view_generator("forced_choice", {
+const forced_choice_2A = babeViews.view_generator("textbox_input", {
     // This will use all trials specified in `data`, you can user a smaller value (for testing), but not a larger value
-    trials: trial_info.forced_choice.length,
+    trials: trial_info.textbox_input.length,
     // name should be identical to the variable name
     name: 'forced_choice_2A',
-    data: trial_info.forced_choice,
+    data: trial_info.textbox_input,
     // you can add custom functions at different stages through a view's life cycle
     // hook: {
     //     after_response_enabled: check_response
